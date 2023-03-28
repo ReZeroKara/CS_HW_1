cc7df5f7b3de839e14d3f87d47ccbb9f0571a314
ReZero <ghostofpast@icloud.com>
2023-03-28 23:05:01 +0300

    add task001 and gitignore

---
+++ task001_MoreLess/Program.cs
@@ -0,0 +1,26 @@
+﻿//Программа принимает на вход два числа и выдаёт, какое из них больше, а какое меньше.
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Max: " + a);
+    Console.WriteLine("Min: " + b);
+}
+else if (b > a)
+{
+    Console.WriteLine("Max: " + b);
+    Console.WriteLine("Min: " + a);
+}
+else if (a == b)
+{
+    Console.WriteLine("Числа равны!");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод!");
+}
