b3a5306270ec0e4be7812b7df94b1f68a9d54698
ReZero <ghostofpast@icloud.com>
2023-03-28 23:42:05 +0300

    add task004

---
+++ task004_EvenNum/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int count1 = 0;
+int count2 = n - 1;
+int a = 0;
+
+while(count1 < count2)
+{
+    int result = 2 + a;
+    Console.Write(result + ", ");
+    a += 2;
+    count1 += 2;
+}
