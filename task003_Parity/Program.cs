819550d52e5ab37c12ee2e3d5308b21574893e4d
ReZero <ghostofpast@icloud.com>
2023-03-28 23:29:52 +0300

    add task003

---
+++ task003_Parity/Program.cs
@@ -0,0 +1,14 @@
+﻿//Программа определяет, чётное число или нет.
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a % 2;
+
+if (b == 0)
+{
+    Console.WriteLine("Число является чётным!");
+}
+else
+{
+    Console.WriteLine("Число не является чётным!");
+}
